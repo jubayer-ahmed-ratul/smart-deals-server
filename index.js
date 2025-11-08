760d9d7ba15fefc3610e43cedbaec2d45f781edd
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2025-11-08 13:00:55 +0600

    environ variable added

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 
+// console.log(process.env)
+
+
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-const uri =
-  "mongodb+srv://smartdbUser:JdCAvdc3VFO3NlGV@cluster0.bsfywqv.mongodb.net/?appName=Cluster0";
+// const uri =
+//   "mongodb+srv://smartdbUser:JdCAvdc3VFO3NlGV@cluster0.bsfywqv.mongodb.net/?appName=Cluster0";
+
+  const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bsfywqv.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
